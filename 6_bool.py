4e44a3bc55814702b7853ae29f1000004b438b4b
kyu-jung <bongseo1012@gmail.com>
2025-03-27 15:54:03 +0900

    Add files via upload

---
+++ 6_bool.py
@@ -0,0 +1,23 @@
+# bool  참과 거짓을 나타냄
+a = [1,2,3,4] # 리스트 안에 요소가 있으면 참이다.
+while a :
+    print(a.pop()) # pop이 진행되는 동안 참이었다가 마지막에 []안에 요소가 다 지워져서 작동을 멈춤.
+    print(a)
+
+
+if[] :
+    print('참')
+
+else:
+    print('거짓')
+
+
+
+if[1,2,3] :
+    print('참')
+
+else:
+    print('거짓')
+
+
+print(bool(''))
